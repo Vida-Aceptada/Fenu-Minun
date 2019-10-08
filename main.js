dade8b292b2b62018efe2a28ca393e79779235b5
Vida-Aceptada <vida.aceptada@gmail.com>
2019-10-08 10:55:58 -0700

    Mobile styling for Song Search project's ui

--- main.js
+++ main.js
@@ -57,7 +57,6 @@ function projectViewer(project) {
 	}
 	let fade = setInterval(() => {
 		startOpacity = parseFloat(Number.parseFloat(startOpacity + counter/10).toFixed(1));
-		console.log(startOpacity);
 		viewer.style.opacity = startOpacity.toString();
 		if ((!isOpen && startOpacity >= 1) || (isOpen && startOpacity <= 0)) {
 			if (isOpen) {
@@ -69,48 +68,3 @@ function projectViewer(project) {
 		}
 	}, 30); // the speed of the fade is set here
 }
-
-
-/*
-function dragElement(elmnt) {
-	let pos1 = 0,
-		pos2 = 0,
-		pos3 = 0,
-		pos4 = 0;
-	
-	if (document.getElementById('DRAG_BAR')) {
-		document.getElementById('DRAG_BAR').onmousedown = dragMouseDown;
-	} else {
-		elmnt.onmousedown = dragMouseDown;
-	}
-	
-	function dragMouseDown(e) {
-		e = e || window.event;
-		e.preventDefault();
-		// get the mouse cursor position at startup:
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		document.onmouseup = closeDragElement;
-		// call a function whenever the cursor moves:
-		document.onmousemove = elementDrag;
-	}
-	
-	function elementDrag(e) {
-		e = e || window.event;
-		e.preventDefault();
-		// calculate the new cursor position:
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		// set the element's new position:
-		elmnt.style.top = `${elmnt.offsetTop - pos2}px`;
-		elmnt.style.left = `${elmnt.offsetLeft - pos1}px`;
-	}
-	
-	function closeDragElement() {
-		// stop moving when mouse button is released:
-		document.onmouseup = null;
-		document.onmousemove = null;
-	}
-}*/
